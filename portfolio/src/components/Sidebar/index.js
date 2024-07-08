66371774d3894a9cd41e15bebb4a4dafd2b5554e
dwijeshbing <dwijeshbingumalla@gmail.com>
2024-07-08 21:09:06 +0530

    abc

---
+++ portfolio/src/components/Sidebar/index.js
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom';
+import './index.scss';
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <div className='nav-bar'>
+      
+    </div>
+  )
+}
+
+export default Sidebar
